bcb6a17824a314ed5f53d75212ffecb526013fe5
Ivsucram <ivsucram@gmail.com>
2024-09-09 00:50:52 +0800

    Refactor project structure to follow java package naming convention

--- src/main/kotlin/sg/com/quantai/etl/GreeterController.kt
+++ src/main/kotlin/sg/com/quantai/etl/GreeterController.kt
@@ -1,4 +1,4 @@
-package monolith
+package sg.com.quantai.etl
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
---
+++ src/test/kotlin/sg/com/quantai/etl/S3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package sg.com.quantai.etl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ETLApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
