66a8a72e905ab1ca25989a02adf53ce3f481eb9c
Geng Yang <CGY1539@users.noreply.github.com>
2026-02-24 21:40:11 +0800

    throws and error instead of return null

--- src/main/kotlin/sg/com/quantai/etl/services/ForexService.kt
+++ src/main/kotlin/sg/com/quantai/etl/services/ForexService.kt
@@ -270,7 +270,7 @@ class ForexService(
 
         } catch (e: Exception) {
             logger.error("Error fetching forex data by date for $currencyPair: ${e.message}")
-            null
+            throw RuntimeException("Failed to fetch historical data for $currencyPair", e)
         }
     }
 
