e028e8b14c74b5f06f1d60d6b3091b99acd7d0a8
syLuar <110336766+syLuar@users.noreply.github.com>
2026-04-23 11:05:33 +0800

    Quantntu 79 (#24)
    * fix: changed hourly agg init logic to hours instead of days
    
    * fix: missing @Test; wrong assert string fixed
    
    * fix: small doc discrepancy
    
    * cleanup: unused imports, functions

--- src/main/kotlin/sg/com/quantai/etl/controllers/DataWarehouseController.kt
+++ src/main/kotlin/sg/com/quantai/etl/controllers/DataWarehouseController.kt
@@ -2,12 +2,10 @@ package sg.com.quantai.etl.controllers
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import sg.com.quantai.etl.services.DataWarehouseETLService
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/v1/warehouse")
